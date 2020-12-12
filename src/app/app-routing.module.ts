2c93a307650a4219e34f871842b8c40d1d56e8ae
patrickqcf <45515053+patrickqcf@users.noreply.github.com>
2020-12-12 19:50:02 -0300

    Exportando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
+import { pairs } from 'rxjs';
+
+import { TarefaRoutes } from "./tarefas";
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tarefas/listar',
+    pathMatch: 'full'
+  },
+  
+  // o operador '...' concatena um array filho com o pai, portanto, como o TarefaRoutes é um array de rotas em JSON e o Routes TAMBÉM é um array de rotas
+  // foi optado apenas concatenar um com o outro com esse operador, interessante.
+ ...TarefaRoutes 
 
 ];
 @NgModule({
