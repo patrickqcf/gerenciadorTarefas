b6ccb924b15b7d1b9dab1a00aaa3d860be99cf6f
patrickqcf <45515053+patrickqcf@users.noreply.github.com>
2020-12-12 16:22:01 -0300

    Comit configuração bootstrap e rotas

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,12 +7,7 @@ describe('workspace-project App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
-
-  it('should display welcome message', async () => {
-    await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('gerenciadorTarefas app is running!');
-  });
-
+  
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
